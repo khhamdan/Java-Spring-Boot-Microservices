6557696cdf21edd1f97685b98df9aac83e8616da
Hamdan <hamdanahmadkhawaja@gmail.com>
2025-11-03 17:03:28 +0500

    Setup Eureka Server for Service Registry and Inter communication

---
+++ service-rg/src/test/java/com/microservice/servicerg/ServiceRgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.servicerg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
